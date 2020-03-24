fc510246fae320355ad54af761e3e1b2d3dade4a
anmariyafrancis <60163786+anmariyafrancis@users.noreply.github.com>
2020-03-24 18:48:06 -0400

    Update ViewController.swift
    next button added

--- WebViewExample/ViewController.swift
+++ WebViewExample/ViewController.swift
@@ -7,13 +7,57 @@
 //
 
 import UIKit
-
+//Need to use
+import WebKit
 class ViewController: UIViewController {
+   
+    @IBOutlet weak var myWebKitView: WKWebView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+         super.viewDidLoad()
+         loadLambtonUrl()
         // Do any additional setup after loading the view.
     }
+    func loadLambtonUrl()
+    {
+        let url = URL(string: "https://www.lambtoncollege.ca/")
+        let urlReq = URLRequest(url: url!)
+        myWebKitView.load(urlReq)
+    }
+     func loadHtmlString()
+    {
+        let htmlString = "<h1>Hello World</h1>"
+        myWebKitView.loadHTMLString(htmlString, baseURL: nil)
+        
+    }
+     @IBAction func btnNavigation(_ sender: UIBarButtonItem)
+    {
+        switch sender.tag {
+        case 0://Home Button is pressed
+            myWebKitView.reloadFromOrigin()
+        case 1://Prev Button is pressed
+            if myWebKitView.canGoBack
+            {
+                print("Go back")
+                myWebKitView.goBack()
+            }else
+            {
+                print("Can't Go back")
+            }
+        case 2://next button is pressed
+            if myWebKitView.canGoForward
+            {
+                print("Go Next")
+                myWebKitView.goForward()
+            }else
+            {
+                print("Can't Go Forward")
+            }
+        default:
+            print("No Navigation action found...")
+        }
+    
 
 
 }
